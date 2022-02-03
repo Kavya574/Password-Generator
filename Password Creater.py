4d03a229bf7f5e095a2ad1a78b597bc94a557626
Kavya R <92448028+Kavya574@users.noreply.github.com>
2022-02-03 18:23:20 +0530

    Add files via upload

---
+++ Password Creater.py
@@ -0,0 +1,26 @@
+# import the needed library
+import random
+import string
+
+# Welcome message to the user
+print("Hello, Welcome to Password Generator by Kavi")
+
+# Ask the users for the length of the password
+length = int(input("Enter the length of the password: "))
+
+#define data using string module
+lower = string.ascii_letters
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+# combine the data and store it all new variable
+all = lower+upper+num+symbols
+
+#Use random module to generate password
+temp = random.sample(all,length)
+password = "".join(temp)
+
+#At last print password
+print(password)
+
